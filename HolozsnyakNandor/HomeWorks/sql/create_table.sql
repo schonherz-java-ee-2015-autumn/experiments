7e6330a3c2e1c8306ae012416a00c790fd3ab9a4
Nándor Holozsnyák <holi60@gmail.com>
2015-10-30 12:39:14 +0100

    SQL hazi, sajnos a 3NF nem sikerült

---
+++ HolozsnyakNandor/HomeWorks/sql/create_table.sql
@@ -0,0 +1,19 @@
+DROP TABLE if exists importFromCSV;
+CREATE TABLE importFromCSV(
+    customer_name varchar(8),
+    city varchar(100),
+    street varchar(50),
+    house_number varchar(10),
+    floor_number varchar(10),
+    door_number varchar(10),
+    bell_name varchar(30),
+    company_name varchar(100),
+    tel_number1 varchar (15),
+    tel_number2 varchar (15),
+    tel_number3 varchar (15),
+    comment varchar(100),
+    discount_percentage varchar(3),
+    banned varchar(2),
+    premium varchar(2),
+    number_of_orders int
+);
