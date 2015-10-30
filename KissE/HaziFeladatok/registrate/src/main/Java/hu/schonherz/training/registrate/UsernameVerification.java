109d8612c161bedf683339f37b00b93752dcd019
Kisse15 <stephanuss@gmail.com>
2015-10-30 14:27:45 +0100

    házi feladatok mappa

---
+++ KissE/HaziFeladatok/registrate/src/main/Java/hu/schonherz/training/registrate/UsernameVerification.java
@@ -0,0 +1,14 @@
+package hu.schonherz.training.registrate;
+
+
+import java.util.ArrayList;
+
+public class UsernameVerification {
+	public static ArrayList<String> usernames = new ArrayList<String>();
+	
+	public void addUserToUsernames(String s){
+		usernames.add(s);
+	}
+	
+	
+}
